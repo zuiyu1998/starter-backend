31087e76ab904e6683ce9c13347824c5fab52e02
zuiyu1998 <1542844298@qq.com>
2024-06-25 15:09:22 +0800

    fix: ProjectRepo添加Send和Sync约束

--- db/src/database/project.rs
+++ db/src/database/project.rs
@@ -2,6 +2,6 @@ use crate::Result;
 use abi::{async_trait::async_trait, prelude::*};
 
 #[async_trait]
-pub trait ProjectRepo {
+pub trait ProjectRepo: 'static + Send + Sync {
     async fn get_project_list(&self) -> Result<StarterProjectListResponse>;
 }
