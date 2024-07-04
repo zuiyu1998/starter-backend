4a2f53ce374fef54b8efaac364e9f1819d2e19af
zuiyu1998 <1542844298@qq.com>
2024-07-04 11:45:58 +0800

    feat: TagIndexRepo 添加'static + Send + Sync 约束

--- doc_index/src/tag.rs
+++ doc_index/src/tag.rs
@@ -1,6 +1,6 @@
 use crate::Result;
 
-pub trait TagIndexRepo {
+pub trait TagIndexRepo: 'static + Send + Sync {
     fn store_index(&mut self, tag: &str, id: i32) -> Result<()>;
 
     fn get_indexs(&self, tag: &str, page_size: i32, page: i32) -> Result<Vec<i32>>;
